befeca063a2133b5b83aa64b3746edd90e3590dc
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2013-01-15 12:34:52 +0000

    Updated the methods used to discover PEAR channels

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,19 +1,17 @@
 include_recipe "php"
 
-channel = php_pear_channel "pear.symfony.com" do
-  action :discover
+channels = %w{pear.phpunit.de pear.symfony.com}
+channels.each do |chan|
+  php_pear_channel chan do
+    action [:discover, :update]
+  end
 end
 
-php_pear "YAML" do
-  channel channel.channel_name
-  action :install
-end
-
-channel = php_pear_channel "pear.phpunit.de" do
-  action :discover
+php_pear "PEAR" do
+  action :upgrade
 end
 
 php_pear "PHPUnit" do
-  channel channel.channel_name
+  channel "phpunit"
   action :install
 end
